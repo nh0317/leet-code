baf2cbfb01be2727763cadb8d028fe1b9c2d2500
김나현 <nh2004@naver.com>
2022-08-17 16:37:02 +0900

    Time: 2585 ms (44.36%), Space: 24.3 MB (31.13%) - LeetHub

---
+++ 2028-find-missing-observations/2028-find-missing-observations.py
@@ -0,0 +1,40 @@
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        m = len(rolls)
+        total = mean * (n + m) - sum(rolls)
+        
+        if total > n * 6 or total < 1*n: 
+            return []
+        
+        if total == n * 6:
+            return [6] * n
+        
+        if total == 1 * n:
+            return [1] * n
+        
+        minn = total // n
+        
+        result = [minn for _ in range(n)]
+        
+        summ = sum(result)
+        idx = 0
+        while summ <= n * 6:
+            if summ == total:
+                return result
+            elif summ > total:
+                if idx < n:
+                    if result[idx] <= 1:
+                        idx += 1
+                    else: 
+                        result[idx] -= 1
+                        summ -= 1
+                else: return []
+            else:
+                if idx < n:
+                    if result[idx] >= 6:
+                        idx += 1
+                    else: 
+                        result[idx] += 1
+                        summ += 1
+                else: return []
+        

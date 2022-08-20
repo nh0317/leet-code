6588b2758310598b5b47597771d499573ea03587
김나현 <nh2004@naver.com>
2022-08-20 15:15:15 +0900

    Time: 73 ms (20.16%), Space: 13.8 MB (79.75%) - LeetHub

--- 61-rotate-list/61-rotate-list.py
+++ 61-rotate-list/61-rotate-list.py
@@ -4,39 +4,49 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if not head:
-            return head
-        
+    def node_length(self, head: Optional[ListNode]):
         tail = head
         n = 1
         while tail.next:
             n += 1
             tail = tail.next
-        if n < k:
-            k = k % n
-            
-        if k == 0:
-            return head
-        
+        return n
+    
+    def move(self, head: Optional[ListNode], k: int):
         tail = head
-        for _ in range(n-k):
+        for _ in range(k):
             tail = tail.next
-            
-        node = ListNode()
-        node.val = tail.val
-        node.next = tail.next
+        return tail
         
+    def append(self, node: Optional[ListNode], head: Optional[ListNode]):
         cur = node
-        
         while node.next:
             node = node.next
-            
         node.next = head
-        
-        for _ in range(n-k):
-            node = node.next
-            
-        node.next = None
-        
         return cur
+        
+    #k 이후 전부 삭제
+    def remove(self, node: Optional[ListNode], k:int):
+        cur = node
+        node = self.move(node,k-1)
+        node.next = None
+        return cur
+        
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return head
+        
+        n = self.node_length(head)
+        
+        if n < k:
+            k = k % n
+        if k == 0 or n-k == 0:
+            return head
+        
+        tail = self.move(head, n-k)
+            
+        node = ListNode(tail.val, tail.next)
+        head = self.remove(head, n-k)
+        node = self.append(node, head)
+        
+        return node

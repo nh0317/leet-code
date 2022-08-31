6de536c9f7576112baf21b1c29c7fdb3e32b22e2
김나현 <nh2004@naver.com>
2022-08-31 13:19:46 +0900

    Time: 450 ms (75.77%), Space: 0B (100.00%) - LeetHub

--- 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -2,7 +2,5 @@
 select customer_number
 from Orders
 group by customer_number
-having count(customer_number) = (
-    select max(cnt)
-    from (select count(*) as cnt from Orders group by customer_number) t
-)
+order by count(customer_number) desc
+limit 1

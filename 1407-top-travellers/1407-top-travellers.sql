fe58205986b58ac0f06bafaf856cc05971bec80c
김나현 <nh2004@naver.com>
2022-09-08 11:59:49 +0900

    Time: 1927 ms (6.22%), Space: 0B (100.00%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,16 +1,5 @@
 # Write your MySQL query statement below
-select u.name, 
-    (case
-        when r.distance is null then 0
-        else sum(r.distance)
-    end
- )as travelled_distance 
+select u.name, ifnull(sum(r.distance), 0) as travelled_distance 
 from Users u left join Rides r on u.id = r.user_id
 group by u.id
 order by travelled_distance desc, name asc
-# (
-#     case
-#         when count(distance) >= 2 then name
-#         else distance
-#     end
-# )

2af4cc83934d5ac15f09e8d9524c1901b88194e7
김나현 <nh2004@naver.com>
2022-09-11 15:01:45 +0900

    Time: 2027 ms (34.65%), Space: 36.3 MB (57.49%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -1,20 +1,17 @@
 class Solution:
-    def travel_home(self, garbage, travel, g_type):
-        cnt = 0
-        g = 0
-        for i in range(len(garbage)):
-            if garbage[i].count(g_type):
-                while g < i:
-                    cnt += travel[g]
-                    g += 1
-                cnt += garbage[i].count(g_type)
-        return cnt
-        
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
+        cnt = 0
+        g, p, m = 0, 0, 0
+        for i in range(len(garbage)):
+            if "G" in garbage[i]:
+                g = i
+            if "P" in garbage[i]:
+                p = i
+            if "M" in garbage[i]:
+                m = i
+            cnt += len(garbage[i])
         
-        cnt = self.travel_home(garbage, travel, "G")
-        cnt += self.travel_home(garbage, travel, "P")
-        cnt += self.travel_home(garbage, travel, "M")
-                
+        for i in [g,p,m]:
+            cnt += sum(travel[:i])
         return cnt 
                 

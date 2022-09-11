3b9051ebf54aea89d0d258adf4de37cdae6c4414
김나현 <nh2004@naver.com>
2022-09-11 11:03:23 +0900

    Time: 384 ms (53.57%), Space: 36.6 MB (76.43%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.kt
@@ -0,0 +1,7 @@
+import java.math.BigInteger
+
+class Solution {
+    fun multiply(num1: String, num2: String): String {
+        return (num1.toBigInteger() * num2.toBigInteger()).toString()
+    }
+}

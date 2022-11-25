2004a6b4482c0a2df44284b85687e93dd40c129f
김나현 <nh2004@naver.com>
2022-11-25 11:43:53 +0900

    Time: 654 ms (85.08%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-select user_id, max(time_stamp) as last_stamp 
-from Logins 
-where year(time_stamp) = 2020
+
+select user_id, max(time_stamp) as last_stamp
+from Logins
+where Year(time_stamp) = 2020
 group by user_id
